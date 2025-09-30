9ddb2a5d60559203fc1356f84972b2ff0da33c67
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-09-30 21:42:39 +0500

    Add project files.

---
+++ Tyuiu.KolganenkoRV.Sprint2.Task0.V26.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KolganenkoRV.Sprint2.Task0.V26.Lib
+{
+    public class DataService : ISprint2Task0V26
+    {
+        // x=1045 y=975
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y; //f
+            res[1] = x != y + 70; //f
+            res[2] = x < y  ; //f
+            res[3] = x > y; //t
+            res[4] = x <= y + 70; //t
+            res[5] = x >= y; //t
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint2.Task0.V26/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.KolganenkoRV.Sprint2.Task0.V26.Lib;
+DataService ds = new DataService();
+
+int x = 1045;
+int y = 975;
+bool[] res = new bool[6];
+res = ds.GetCompareOperations(x, y);
+
+Console.Title = "Спринт #2 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #0                                                              *");
+Console.WriteLine("* Вариант #26                                                             *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу из операций сравнений                                *");
+Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)*"); 
+Console.WriteLine("* и арифметических выражений, которая вернет логическую                   *");
+Console.WriteLine("* последовательность(массив): (False, False, False, True, True, True),    *");
+Console.WriteLine("* при x = 1045, y = 975                                                   *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("X = " + x);
+Console.WriteLine("Y = " + y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
+
+     
