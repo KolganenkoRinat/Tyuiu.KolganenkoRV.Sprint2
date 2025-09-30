b73445977f29a051c37a833ec05eeed167982a7a
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-01 01:23:24 +0500

    Task1

---
+++ Tyuiu.KolganenkoRV.Sprint2.Task1.V29.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.KolganenkoRV.Sprint2.Task1.V29.Lib
+{
+    public class DataService : ISprint2Task1V29
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (c < d); //t +
+            res[1] = (a + 2 > b) & (c < d + 1); //t + 
+            res[2] = (a > b) || (c < d) ; //t + 
+            res[3] = !res[1]; //f +
+            res[4] = (a + 2 > b) && (c < d + 1); //t + 
+            res[5] = (a > b) ^ (c < d); //t + 
+
+            return res;
+        }
+    }
+}
+//a = 657
+//b = 654
+//c = 657
+//d = 657
---
+++ Tyuiu.KolganenkoRV.Sprint2.Task1.V29/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.KolganenkoRV.Sprint2.Task1.V29.Lib;
+
+DataService ds = new DataService();
+
+int a = 657;
+int b = 654;
+int c = 657;
+int d = 657;
+bool[] res = new bool[6];
+res = ds.GetLogicOperations(a, b, c, d);
+
+Console.Title = "Спринт #2 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #29                                                             *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу из операций сравнений                                *");
+Console.WriteLine("* (==, !=, <, >, <=, >=, последовательность можно чередовать,             *");
+Console.WriteLine("* но использовать один раз в выражении) и логических операций             *");
+Console.WriteLine("* (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться)  *");
+Console.WriteLine("* последовательность(массив): (True, True, True, False, True, True),      *");
+Console.WriteLine("* при a = 657, b = 654, c = 657, int d = 657                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("A = " + a);
+Console.WriteLine("B = " + b);
+Console.WriteLine("C = " + c);
+Console.WriteLine("D = " + d);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine(res[i]);
+}
+Console.ReadKey();
+
+
