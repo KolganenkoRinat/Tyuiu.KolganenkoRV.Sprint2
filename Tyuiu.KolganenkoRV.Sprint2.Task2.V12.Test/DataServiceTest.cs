94c42815540163cd5d7138449fc91c95357ac47b
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-02 20:57:59 +0500

    Task2

---
+++ Tyuiu.KolganenkoRV.Sprint2.Task2.V12.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KolganenkoRV.Sprint2.Task2.V12.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint2.Task2.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest 
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 4;
+            int y = 5;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint2.Task2.V12.Test/ISprint1Task2V12.cs
@@ -0,0 +1,6 @@
+﻿namespace Tyuiu.KolganenkoRV.Sprint2.Task2.V12.Test
+{
+    public interface ISprint1Task2V12
+    {
+    }
+}
