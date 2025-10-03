7069dac60f7c08ac2724017edec6e6970aa65133
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-04 00:29:53 +0500

    Task4

---
+++ Tyuiu.KolganenkoRV.Sprint2.Task4.V12.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KolganenkoRV.Sprint2.Task4.V12.Lib;
+
+namespace Tyuiu.KolganenkoRV.Sprint2.Task4.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 4;
+            double y = 1;
+            double wait = 4.324;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);          
+        }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 36;
+            double y = 4;
+            double wait = 144.383;
+            double res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.KolganenkoRV.Sprint2.Task4.V12/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KolganenkoRV.Sprint2.Task4.V12.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #1 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #1                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #12                                                             *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Z с    *");
+Console.WriteLine("* использованием тернарного оператора, где пользователь вводит значения   *");
+Console.WriteLine("* переменной X и Y с клавиатуры. Округлить полученное значение до 3       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите значение X:");
+double x = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите значение Y:");
+double y = Convert.ToDouble(Console.ReadLine());
+
+
+double res = ds.Calculate(x, y);
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Значение функции = " + res);
+Console.ReadLine();
