34a71d027bd63851c5bd6b38877deedcf2402a9a
Tyuiu.KolganenkoRV@outlook.com <kolganenko.rinat@mail.ru>
2025-10-04 13:18:59 +0500

    Task5

---
+++ Tyuiu.KolganenkoRV.Sprint2.Task5.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KolganenkoRV.Sprint2.Task5.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила : Колганенко Р. В. | ИБКСб-25-1";
+//Длина строки 75 символов
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #3                                                             *");
+Console.WriteLine("* Выполнил: Колганенко Ринат Витальевич | ИБКСб-25-1                      *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("*    *");
+Console.WriteLine("*  *");
+Console.WriteLine("*       *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите день недели:");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+string res;
+
+if ((numDay > 7) || (numDay < 1))
+{
+    res = ("Нет такого дня недели");
+}
+else
+{
+    res = ("Это " + ds.FindDayName(numDay));
+}
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
+
+Console.ReadLine();
